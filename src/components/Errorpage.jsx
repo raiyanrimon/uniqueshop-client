c654d5eca7fea6fccb4c62315af7d93053cb8da3
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2023-10-19 02:06:45 +0600

    first commit

---
+++ src/components/Errorpage.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import ErrorPic from "../assets/Scarecrow.png";
+import Navbar from "./Navbar";
+
+const Errorpage = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <div className="container text-center my-10">
+        <div className="flex justify-center items-center">
+          <img src={ErrorPic} alt="" />
+        </div>
+        <div className="space-y-5">
+          <h3 className="font-bold text-3xl my-5">I have bad news for you </h3>
+          <p className="font-medium text-xl mb-5">
+            The page you are looking for might be removed or is temporarily
+            unavailable
+          </p>
+          <Link to="/">
+            <button className="btn-primary btn mt-5">Back to homepage</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Errorpage;
---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root";
+import Errorpage from "./components/Errorpage";
+import Home from "./components/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <Errorpage></Errorpage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
