a1fa7a3ad51683cd1c4277e5dcf5f98e86185b5b
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2023-10-20 18:47:04 +0600

    slider added

--- src/components/BrandPage.jsx
+++ src/components/BrandPage.jsx
@@ -1,5 +1,11 @@
 import { useLoaderData } from "react-router-dom";
 import ProductCard from "./ProductCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const BrandPage = () => {
   const brands = useLoaderData();
@@ -12,10 +18,28 @@ const BrandPage = () => {
   }
   console.log(brands);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-      {brands.map((brand) => (
-        <ProductCard key={brand._id} brand={brand}></ProductCard>
-      ))}
+    <div className="space-y-10">
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        {brands.map((brand) => (
+          <SwiperSlide key={brand._id}>
+            <img width={800} height={100} src={brand.image} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+        {brands.map((brand) => (
+          <ProductCard key={brand._id} brand={brand}></ProductCard>
+        ))}
+      </div>
     </div>
   );
 };
